805136006cd08bc3079517de87c470f2308c8271
Jesus Perdomo <japerdomo08@gmail.com>
2023-12-16 12:08:06 +0000

    Act 16-12-23

---
+++ js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+  $('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const menuIcon = document.getElementById('menu_icon');
+  const menuBar = document.getElementById('menu_bar');
+
+  menuIcon.addEventListener('click', function () {
+    menuBar.classList.toggle('active');
+  });
+});
+
+
--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuIcon = document.getElementById('menu-icon');
-    const menu = document.getElementById('menu');
-  
-    menuIcon.addEventListener('click', function () {
-      menu.classList.toggle('active');
-    });
-  });
